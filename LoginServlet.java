befb4319a4d72954789d12f882324f7127e221c6
alok9616 <168061197+alok9616@users.noreply.github.com>
2025-01-21 10:59:54 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,42 @@
+package com.company.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+import com.company.dao.UserDaoImp;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private static UserDaoImp userDao = new UserDaoImp();       
+   
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (userDao.isValidUser(username, password)) {
+         HttpSession session = request.getSession();
+           session.setAttribute("username", username);
+            response.sendRedirect("welcome.jsp");
+           //System.out.println("Hi - "+username);
+        } 
+//        if(username.equals("alok")){
+//        	response.sendRedirect("welcome.jsp");
+//        	
+//        }
+        else {
+            response.sendRedirect("login.jsp?error=1");
+        	System.out.println("Error A gya");
+        }
+	}
+
+}
---
+++ logout.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ page import="jakarta.servlet.http.HttpSession" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+          HttpSession currentSession = request.getSession(false);
+          if(currentSession != null){
+        	  currentSession.invalidate();
+          }
+          
+          response.sendRedirect("index.html");
+          
+%>
+
+</body>
+</html>
